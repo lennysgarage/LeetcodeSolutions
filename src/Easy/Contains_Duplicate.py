58a83d79859d14ce427bd7e2873a233bb087756e
Jonathan <lennysgarage1@gmail.com>
2021-09-02 15:15:39 -0400

    Add simple test code

--- src/Easy/Contains_Duplicate.py
+++ src/Easy/Contains_Duplicate.py
@@ -3,3 +3,11 @@ from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
+
+    
+
+
+
+if __name__ == "__main__":
+    assert(Solution().containsDuplicate([1,2,3,4,1]) == True) # Duplicate
+    assert(Solution().containsDuplicate([1,2,3,4]) == False) # No Duplicate
