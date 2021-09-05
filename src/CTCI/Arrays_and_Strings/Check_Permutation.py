f35a0b579912ed992a5fbd20cd87d120e98688d3
lennysgarage <lennysgarage1@gmail.com>
2021-09-05 18:36:40 -0400

    fix: update test cases

--- src/CTCI/Arrays_and_Strings/Check_Permutation.py
+++ src/CTCI/Arrays_and_Strings/Check_Permutation.py
@@ -1,28 +1,63 @@
-from typing import List
+import unittest
+from collections import Counter
+
 
 class Solution:
     def determine_permutation(self, s1: str, s2: str):
-        if len(s1) != len(s2): # Permutation requires the strings to have the same length
+        if len(s1) != len(s2):  # Permutation requires the strings to have the same length
             return False
 
-        letters = {} # If we know size of character set like ASCII we could make a list of [None] * size
+        letters = {}  # If we know size of character set like ASCII we could make a list of [None] * size
         # Get frequency of each character in first string
         for char in s1:
             if char in letters:
                 letters[char] += 1
             else:
                 letters[char] = 1
-        
+
         # Check if every character in second string is also a character in first
         for char in s2:
             if char not in letters:
                 return False
             letters[char] -= 1
             if letters[char] < 0:
-                return False # We need more letters than available
+                return False  # We need more letters than available
         return True
-            
+
+    def determine_permutation_pythonic(self, s1: str, s2: str):
+        if len(s1) != len(s2):
+            return False
+
+        return Counter(s1) == Counter(s2)
+
+
+class Test(unittest.TestCase):
+    test_cases = [
+        ("abcae", "fbcaa", False),
+        ("aabc", "abac", True),
+        ("dog", "god", True),
+        ("abcd", "bacd", True),
+        ("3563476", "7334566", True),
+        ("wef34f", "wffe34", True),
+        ("dogx", "godz", False),
+        ("abcd", "d2cba", False),
+        ("2354", "1234", False),
+        ("dcw4f", "dcw5f", False),
+        ("DOG", "dog", False),
+        ("dog ", "dog", False),
+        ("aaab", "bbba", False),
+    ]
+    test_functions = [
+        Solution().determine_permutation,
+        Solution().determine_permutation_pythonic,
+    ]
+
+    def test_cp(self):
+        # true check
+        for check_permutation in self.test_functions:
+            for str1, str2, expected in self.test_cases:
+                assert check_permutation(str1, str2) == expected
+
 
 if __name__ == "__main__":
-    assert(Solution().determine_permutation('abcae', 'fbcaa') == False)
-    assert(Solution().determine_permutation('aabc', 'abac') == True)
+    unittest.main()
--- src/CTCI/Arrays_and_Strings/Is_Unique.py
+++ src/CTCI/Arrays_and_Strings/Is_Unique.py
@@ -1,22 +1,56 @@
-from typing import List
+import time
+import unittest
+from collections import defaultdict
+
 
 class Solution:
     def determine_unique(self, s: str):
         d = {}
         for char in s:
             if char in d:
-                return False # Contains a duplicate
+                return False  # Contains a duplicate
             else:
                 d[char] = char
         return True
-    
+
     def determine_unique_set(self, s: str):
         return len(set(s)) == len(s)
 
 
+class Test(unittest.TestCase):
+    test_cases = [
+        ("abcd", True),
+        ("s4fad", True),
+        ("", True),
+        ("23ds2", False),
+        ("hb 627jh=j ()", False),
+        ("".join([chr(val) for val in range(128)]), True),  # unique 128 chars
+        ("".join([chr(val // 2) for val in range(129)]), False),  # non-unique 129 chars
+    ]
+    test_functions = [
+        Solution().determine_unique,
+        Solution().determine_unique_set,
+    ]
+
+    def test_is_unique(self):
+        num_runs = 10000
+        function_runtimes = defaultdict(float)
+
+        for _ in range(num_runs):
+            for text, expected in self.test_cases:
+                for is_unique_chars in self.test_functions:
+                    start = time.perf_counter()
+                    assert (
+                        is_unique_chars(text) == expected
+                    ), f"{is_unique_chars.__name__} failed for value: {text}"
+                    function_runtimes[is_unique_chars.__name__] += (
+                        time.perf_counter() - start
+                    ) * 1000
+
+        print(f"\n{num_runs} runs")
+        for function_name, runtime in function_runtimes.items():
+            print(f"{function_name}: {runtime:.1f}ms")
+
 
 if __name__ == "__main__":
-    assert(Solution().determine_unique('abcdefg') == True)
-    assert(Solution().determine_unique_set('abcdefg') == True)
-    assert(Solution().determine_unique('abcdefaghi') == False)
-    assert(Solution().determine_unique_set('abcdefaghi') == False)
+    unittest.main()
